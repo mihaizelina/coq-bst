3914358409e02d7a3baac8fa25042a097545b27c
mihaizelina <38731845+mihaizelina@users.noreply.github.com>
2021-03-17 21:32:15 +0100

    occurs halfway

--- bst.v
+++ bst.v
@@ -65,7 +65,8 @@ Fixpoint occurs (n : nat) (T : tree) : Prop :=
   | node lt k rt => (k = n) \/ (occurs n lt) \/ (occurs n rt)
 end.
 
-(* Proofs *)
+
+(* Proofs for Part 1 *)
 
 (* Auxiliary proof that inserting n > n0 into a BST with all elements < n0 preserves the inequality *)
 Theorem grtPreserveBST : forall t : tree, forall n0 : nat, forall n : nat,
@@ -80,33 +81,48 @@ auto.
 -
 simpl.
 case (compare n1 n).
-
+(* Current node *)
 simpl.
-split.
 simpl in H.
-intuition.
-
-split.
-simpl in H.
-intuition.
-simpl in H.
-intuition.
-
+trivial.
+(* Right subtree (greater) *)
 simpl.
-split.
 simpl in H.
-intuition.
+intuition. (* Each disjunct corresponds to a conclusion *)
+
+(** Complete proof:
 split.
-simpl in H.
-intuition.
+destruct H.
+trivial.
+
+split.
 destruct H.
 destruct H1.
-apply IHt2.
-intuition.
+trivial.
 
+destruct H.
+apply IHt2.
+destruct H1.
+trivial. *)
+(* Left subtree (less) *)
 simpl.
 simpl in H.
 intuition.
+
+(** Complete proof:
+split.
+destruct H.
+trivial.
+
+split.
+destruct H.
+destruct H1.
+apply IHt1.
+trivial.
+
+destruct H.
+destruct H1.
+trivial. *)
 Qed.
 
 (* Analog proof for n < n0 *)
@@ -122,35 +138,19 @@ auto.
 -
 simpl.
 case (compare n1 n).
-
+(* Current node *)
 simpl.
-split.
 simpl in H.
 intuition.
-
-split.
-simpl in H.
-intuition.
-simpl in H.
-intuition.
-
+(* Right subtree (greater) *)
 simpl.
-split.
 simpl in H.
 intuition.
-split.
-simpl in H.
-intuition.
-destruct H.
-destruct H1.
-apply IHt2.
-intuition.
-
+(* Left subtree (less) *)
 simpl.
 simpl in H.
 intuition.
 Qed.
-
 
 Theorem insertBST : forall t : tree, forall n : nat, bst t -> bst (insert n t).
 Proof.
@@ -204,13 +204,260 @@ firstorder.
 apply lessPreserveBST.
 intuition.
 apply nat_compare_gt in Heqe.
-omega.
+intuition.
 Qed.
 
 
 Theorem sortPreservesBST : forall t : tree, bst (sort t).
+Admitted.
+
+
+
+Theorem insertPreservation : forall t : tree, forall n : nat, forall a : nat, occurs n t \/ n = a <-> occurs n (insert a t).
+Proof.
+unfold iff. 
+split.
+intros.
+- (* => *)
+induction t.
+(* Base case *)
+simpl.
+intuition.
+(* Step case *)
+simpl.
+case (compare n0 a) eqn:Heqe1.
+(* Current node *)
+simpl.
+case (compare n0 n) eqn:Heqe2.
+apply nat_compare_eq in Heqe2.
+intuition.
+apply nat_compare_lt in Heqe2.
+intuition.
+apply nat_compare_eq in Heqe1.
+intuition.
+destruct H.
+simpl in H.
+destruct H.
+intuition.
+destruct H.
+right.
+left.
+trivial.
+right.
+right.
+trivial.
+intuition.
+right.
+left.
+apply nat_compare_gt in Heqe2.
+apply nat_compare_eq in Heqe1.
+omega.
+(* Right subtree (greater) *)
+simpl.
+case (compare n0 n) eqn:Heqe2.
+intuition.
+apply nat_compare_eq in Heqe2.
+left.
+trivial.
+destruct H.
+simpl in H.
+intuition.
+right.
+right.
+apply IHt2.
+right.
+trivial.
+intuition.
+simpl in H0.
+intuition.
+(* Left subtree (less) *)
+simpl.
+case (compare n0 n) eqn:Heqe2.
+intuition.
+apply nat_compare_eq in Heqe2.
+left.
+trivial.
+destruct H.
+simpl in H.
+intuition.
+right.
+left.
+apply IHt1.
+right.
+trivial.
+intuition.
+simpl in H0.
+intuition.
+
+- (* <= *)
+intros.
+induction t.
+(* Base case *)
+simpl in H.
+intuition.
+(* Step case *)
+simpl in H.
+case (compare n0 a) eqn:Heqe1 in H.
+simpl in H.
+case (compare n0 n) eqn:Heqe2 in H.
+right.
+apply nat_compare_eq in Heqe1.
+apply nat_compare_eq in Heqe2.
+omega.
+
+left.
+simpl.
+trivial.
+left.
+simpl.
+trivial.
+simpl in H.
+case (compare n0 n) eqn:Heqe2 in H.
+left.
+simpl.
+apply nat_compare_eq in Heqe2.
+left.
+trivial.
+intuition.
+
+apply nat_compare_lt in Heqe2.
+intuition. (* contradiction *)
+
+left.
+simpl.
+right.
+left.
+trivial.
+
+left.
+simpl.
+right.
+right.
+trivial.
+
+left.
+simpl.
+intuition.
+apply nat_compare_lt in Heqe1.
+apply nat_compare_gt in Heqe2.
+intuition. (* contradiction *)
+
+simpl in H.
+case (compare n0 n) eqn:Heqe2 in H.
+left.
+simpl.
+apply nat_compare_eq in Heqe2.
+left.
+trivial.
+destruct H.
+apply nat_compare_lt in Heqe2.
+intuition.
+destruct H.
+destruct IHt1.
+apply H.
+left.
+simpl.
+apply nat_compare_lt in Heqe2.
+right.
+left.
+apply H0.
+right.
+apply H0.
+left.
+simpl.
+apply nat_compare_lt in Heqe2.
+right.
+right.
+apply H.
+destruct H.
+apply nat_compare_gt in Heqe2.
+intuition. (* contradiction *)
+destruct H.
+destruct IHt1.
+apply H.
+left.
+simpl.
+right.
+intuition.
+intuition.
+left.
+simpl.
+right.
+right.
+apply H.
+Qed.
+
+
+Theorem occursBSTfromList : forall n : nat, forall l : list nat, In n l <-> occurs n (listToBST l).
+Proof.
+intros.
+unfold iff.
+split.
+-
+intros.
+induction l.
+(* Base case *)
+simpl.
+intuition.
+(* Step case *)
+simpl.
+simpl in H.
+apply insertPreservation.
+destruct H.
+right.
+intuition.
+apply IHl in H.
+left.
+apply H.
+-
+intros.
+induction l.
+(* Base case *)
+intuition.
+(* Step case *)
+simpl.
+simpl in H.
+apply insertPreservation in H.
+destruct H.
+apply IHl in H.
+right.
+apply H.
+left.
+intuition.
+Qed.
 
 Theorem occursBST : forall n : nat, forall t : tree, occurs n t <-> occurs n (sort t).
+Proof.
+intros.
+unfold iff.
+split.
+- (* '=>' *)
+intros.
+induction t.
+(* Base case *)
+simpl.
+intuition.
+(* Step case *)
+unfold sort.
+Admitted.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
